3e5cc38ed33d5ed0c15a54f8ff1e5b74252aa0f9
Santiago Salcedo <santiagosalcedo.personal@gmail.com>
2024-11-29 06:08:11 -0500

    arreglado en despliegue

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom'; // Importa BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <BrowserRouter basename="/ToDoReact"> {/* Configura el basename */}
+    <App />
+  </BrowserRouter>
+);
